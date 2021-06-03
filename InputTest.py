e260672e00f77188db8c32e82c9912a047f7b3a2
naththanicha ardsuwan <naththanicha@gmail.com>
2021-06-03 19:06:00 +0700

    input

---
+++ InputTest.py
@@ -0,0 +1,6 @@
+# my first python test
+name = input('what is your name : ')
+number = int(input('What is your number : '))
+print('Hi, %s ' % name)
+print('Number is %d ' % number)
+print('welcome to Python.')
