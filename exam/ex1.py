1e824ada977668b413a6cddd01924aac023c9979
naththanicha ardsuwan <naththanicha@gmail.com>
2021-06-03 21:34:14 +0700

    ex1

--- exam/ex1.py
+++ exam/ex1.py
@@ -1,4 +1,10 @@
+
 h = int(input('Input hour : '))
 m = int(input('Input minuite : '))
 s = int(input('Input seconds : '))
 print((h*3600)+(m*60)+s)
+
+'''
+h,m,s = [int(e) for e in input().split()]
+print('',(str)(h*3600+m*60+s))
+'''
